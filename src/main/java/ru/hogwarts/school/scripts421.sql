661291fae6e00438ee47d38e90f708db2b247421
BounsGame <danil89106@yandex.ru>
2025-10-07 06:13:04 +0700

    SQL управление схемой переделал

--- src/main/java/ru/hogwarts/school/scripts421.sql
+++ src/main/java/ru/hogwarts/school/scripts421.sql
@@ -14,3 +14,20 @@ color VARCHAR UNIQUE,
 studentList ARRAY,
 );
 
+
+//через ALTER
+
+ALTER TABLE student
+ALTER COLUMN name NOT NULL;
+
+ALTER TABLE student
+ADD CONSTRAINT uk_name UNIQUE (name);
+
+ALTER TABLE student
+ADD CONSTRAINT chk_age CHECK (age > 16);
+
+ALTER TABLE student
+ALTER COLUMN age SET DEFAULT 20;
+
+ALTER TABLE faculty
+ADD CONSTRAINT uk_faculty UNIQUE (name, color);
