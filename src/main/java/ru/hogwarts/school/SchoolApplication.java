ba93f5246bac158a56730c7b3d1b6013fd07d977
BounsGame <danil89106@yandex.ru>
2025-11-05 20:49:14 +0700

    SQL логирование

--- src/main/java/ru/hogwarts/school/SchoolApplication.java
+++ src/main/java/ru/hogwarts/school/SchoolApplication.java
@@ -3,11 +3,18 @@ package ru.hogwarts.school;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class SchoolApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SchoolApplication.class, args);
+		SpringApplication app = new SpringApplication(SchoolApplication.class);
+
+		app.setDefaultProperties(Collections.singletonMap(
+				"spring.profiles.active", "dev"
+		));
+		app.run(args);
 	}
 
 }
